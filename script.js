5ed39703a27a494dee7b36b37cf976d666d65cd1
Vanipriya Piramanathan <105115435+Vanipriyaarun@users.noreply.github.com>
2022-10-30 20:50:50 +0000

    Weather app

---
+++ script.js
@@ -0,0 +1,46 @@
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+    let temperatureDescription=document.querySelector('.temperature-description');
+    let temperatureDegree=document.querySelector('.temperature-degree');
+    let locationTimeZone=document.querySelector('.location-timeZone');
+    let locationIcon = document.querySelector('.weather-icon');
+    let temperatureSection=document.querySelector(".temperature");
+    let temperatureSpan=document.querySelector(".temperature span");
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            long=position.coords.longitude;
+            lat=position.coords.latitude;
+            // const proxy= "https://cors-anywhere.herokuapp.com/";
+            // const api=`${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+            const api=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=01e91ebd0526ace3c0dd54b0f2b33abd`
+            fetch(api)
+        .then(response=>{
+            return response.json();
+        })
+        .then(data=>{
+            console.log(data);
+            const {temp}=data.main;
+            const {description,icon} = data.weather[0]
+            let  fahrenheit=Math.floor((temp-273.15)* 9/5 + 32 );
+            temperatureDegree.textContent=fahrenheit;
+            temperatureDescription.textContent=description;
+            locationTimeZone.textContent=`UK/${data.name}`;
+            locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
+            let celsius=(fahrenheit-32)*(5 / 9);
+
+            temperatureSection.addEventListener('click',()=>{
+                if(temperatureSpan.textContent==="F"){
+                    temperatureSpan.textContent="C";
+                    temperatureDegree.textContent=Math.floor(celsius);
+                    
+                }else{
+                    temperatureSpan.textContent="F"
+                    temperatureDegree.textContent=fahrenheit;
+                }
+            })
+        });
+        });  
+    }
+    
+});
